46dba402821c21ba81dd39fb77bf904928384265
Anderson Bockor <andersonbockor@gmail.com>
2021-01-19 20:58:57 -0300

    Criação do projeto

---
+++ src/main/java/com/sistemaa/sistemaa/SistemaAApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaa.sistemaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaa/sistemaa/SistemaAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaa.sistemaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
